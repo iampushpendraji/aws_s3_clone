48af12e3efa1f8ccd68eac71d64ab143606a24c8
Pushpendra Sapkale <116496506+pksapkale@users.noreply.github.com>
2023-10-29 10:34:20 +0530

    added ui

--- aws_s3_clone_server/index.js
+++ aws_s3_clone_server/index.js
@@ -9,21 +9,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import routes from "./routes/routes.mjs";
+import cors from "cors";
 import * as dotenv from "dotenv";
-import path from "path";
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));  // For parsing form data
-
-const __filename = new URL(import.meta.url).pathname; // Use the URL and path modules to get the current directory
-const __dirname = path.dirname(__filename);
-
-app.use('/js', express.static(path.join(__dirname, 'js'))); // Set the "js" folder as the static folder
-app.set("view engine", "ejs");
-app.set("views", path.resolve("./views"));
 
 // Always use routes in last
 // Here in routes we have all our routing configurations
--- js/home.cjs
+++
@@ -1,5 +0,0 @@
-fetch("http://localhost:3000/get-buckets").then(e=>{
-    console.log(e)
-}).then(e => {
-    console.log(e)
-})
