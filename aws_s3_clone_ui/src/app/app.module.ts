bb5ff321263bcf44200a1ff118d42cc3ddfea227
Pushpendra Sapkale <dev.psapkale@gmail.com>
2024-09-27 00:40:47 +0530

    Migrated angular to 18

--- aws_s3_clone_ui/src/app/app.module.ts
+++ aws_s3_clone_ui/src/app/app.module.ts
@@ -3,28 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { CreateBucketModalComponent } from './dialogs/create-bucket-modal/create-bucket-modal.component';
 import { CreateObjectModalComponent } from './dialogs/create-object-modal/create-object-modal.component';
 import { FormsModule } from '@angular/forms';
 import { BucketDescComponent } from './bucket-desc/bucket-desc.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    CreateBucketModalComponent,
-    CreateObjectModalComponent,
-    BucketDescComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        CreateBucketModalComponent,
+        CreateObjectModalComponent,
+        BucketDescComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
